c4287f31f31a503c76cf10db2cb7362c6b86d4da
BorislavLuis <54331824+BorislavLuis@users.noreply.github.com>
2021-09-30 00:11:32 +0300

    Home

--- main.cpp
+++ main.cpp
@@ -15,29 +15,27 @@
 #include "io/keyboard.h"
 #include "io/mouse.h"
 #include "io/joystick.h"
+#include "io/camera.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void proccessInput(GLFWwindow* window);
+void proccessInput(GLFWwindow* window,double dt);
 
 float mixVal = 0.5f;
+unsigned int SCR_WIDTH = 1024;
+unsigned int SCR_HEIGHT = 780;
 
-glm::mat4 mouseTransform = glm::mat4(1.0f);
-glm::mat4 trans = glm::mat4(1.0f);
 Keyboard keyboard;
+Camera camera(glm::vec3(0.0f,0.0f,3.0f));
+double deltaTime = 0.0f;
+float lastFrame = 0.0f;
 
+float x, y, z;
 int main()
 {
 	glm::vec4 vec(1.0f, 1.0f, 1.0f, 1.0f);
-	
-	//trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
-	//trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-	//trans = glm::scale(trans, glm::vec3(0.5f, 0.5f, 0.5f));
 
 	int success;
 	char infoLog[512];
-
-
-	vec = trans * vec;
 	std::cout << vec.x << " " << vec.y << " " << vec.z << " " << std::endl;
 	std::cout << "Hello world" << std::endl;
 	glfwInit();
@@ -49,7 +47,7 @@ int main()
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-	GLFWwindow* window = glfwCreateWindow(1024, 780, "Opengl is the best", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH,SCR_HEIGHT, "Opengl is the best", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Couldn't create window" << std::endl;
@@ -63,14 +61,15 @@ int main()
 		std::cout << "Failed to initialize glad" << std::endl;
 		return -1;
 	}
-	glViewport(0, 0, 1024, 780);
+	glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 	glfwSetKeyCallback(window, Keyboard::keyCallback);
 	glfwSetCursorPosCallback(window, Mouse::cursorPosCallback);
 	glfwSetMouseButtonCallback(window, Mouse::mouseButtonCallback);
 	glfwSetScrollCallback(window, Mouse::mouseWheelCallback);
 
-
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	glEnable(GL_DEPTH_TEST);
 	Shader shader("assets/vertex_core.glsl", "assets/fragment_core.glsl");
 
 	unsigned int texture1,texture2;
@@ -98,7 +97,7 @@ int main()
 	}
 
 	stbi_image_free(data);
-
+	
 	glGenTextures(1, &texture2);
 	glBindTexture(GL_TEXTURE_2D, texture2);
 
@@ -115,21 +114,52 @@ int main()
 	stbi_image_free(data);
 	
 	float vertices[] = {
-		-0.5f,-0.5f,0.0f,   1.0f,1.0f,0.5f, 0.0f,0.0f,
-		-0.5f, 0.5f,0.0f, 1.0f,0.4f,0.0f, 0.0f,1.0f,
-		0.5f,-0.5f,0.0f, 1.0f,0.4f,0.0f, 1.0f,0.0f,
-		0.5f, 0.5f,0.0f,  1.0f,0.4f,0.0f, 1.0f,1.0f
-	};
-	unsigned int indecies[] =
-	{
-		0,1,2,
-		3,1,2
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 	};
 
-	unsigned int VAO, VBO,EBO;
+	unsigned int VAO, VBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
 
 
 	glBindVertexArray(VAO);
@@ -137,15 +167,10 @@ int main()
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5*sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(3 *sizeof(float)));
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5*sizeof(float), (void*)(3 *sizeof(float)));
 	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(6 * sizeof(float)));
-	glEnableVertexAttribArray(2);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indecies), indecies, GL_STATIC_DRAW);
 
 	//trans = glm::rotate(trans, glm::radians(25.0f), glm::vec3(0.0f, 0.0f, 1.0f));
 	
@@ -153,31 +178,51 @@ int main()
 	shader.setInt("texture1", 0);
 	shader.setInt("texture2", 1);
 
+	x = 0.0f;
+	y = 0.0f;
+	z = 3.0f;
+
 	while (!glfwWindowShouldClose(window))
 	{
-
-		proccessInput(window);
+		double currentTime = glfwGetTime();
+		deltaTime - currentTime - lastFrame;
+		lastFrame = currentTime;
+		proccessInput(window,deltaTime);
 
 		glClearColor(0.0, 0.8, 0.4, 1.0);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, texture1);
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, texture2);
 		//trans = glm::rotate(trans, glm::radians((float)glfwGetTime()/100.0f), glm::vec3(0.0f, 0.0f, 1.0f));
 		glBindVertexArray(VAO);
+		glm::mat4 model = glm::mat4(1.0f);
+		glm::mat4 view = glm::mat4(1.0f);
+		glm::mat4 projection = glm::mat4(1.0f);
+
+		model = glm::rotate(model, (float)glfwGetTime() * glm::radians(-55.0f), glm::vec3(0.5f));
+		view = camera.getViewMatrix();
+		projection = glm::perspective(glm::radians(camera.zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 1000.0f);
+
+		
 		shader.activate();
 		shader.setFloat("mixValue", mixVal);
-		shader.setMat4("transform", trans);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT,0);
-
+		shader.setMat4("model", model);
+		shader.setMat4("view", view);
+		shader.setMat4("projection", projection);
+		
+		
+		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT,0);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		
+		glBindVertexArray(0);
 		glfwPollEvents();
 		glfwSwapBuffers(window);
 		
 	}
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
-	glDeleteBuffers(1, &EBO);
 	glfwTerminate();
 	return 0;
 }
@@ -185,8 +230,10 @@ int main()
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
+	SCR_WIDTH = width;
+	SCR_HEIGHT = height;
 }
-void proccessInput(GLFWwindow* window)
+void proccessInput(GLFWwindow* window,double dt)
 {
 	if (Keyboard::key(GLFW_KEY_ESCAPE) == GLFW_PRESS)
 	{
@@ -200,21 +247,41 @@ void proccessInput(GLFWwindow* window)
 	{
 		mixVal -= 0.1f;
 	}
-	if (Keyboard::keyWentDown(GLFW_KEY_W))
+	if (Keyboard::key(GLFW_KEY_W))
 	{
-		trans = glm::translate(trans, glm::vec3(0.0f, 0.01f, 0.0f));
+		camera.updateCameraPos(CameraDirection::FORWARD, dt);
+	}
+	if (Keyboard::key(GLFW_KEY_S))
+	{
+		camera.updateCameraPos(CameraDirection::BACKWARD, dt);
+	}
+	if (Keyboard::key(GLFW_KEY_D))
+	{
+		camera.updateCameraPos(CameraDirection::RIGHT, dt);
+	}
+	if (Keyboard::key(GLFW_KEY_A))
+	{
+		camera.updateCameraPos(CameraDirection::LEFT, dt);
+	}
+	if (Keyboard::key(GLFW_KEY_SPACE))
+	{
+		camera.updateCameraPos(CameraDirection::UP, dt);
+	}
+	if (Keyboard::key(GLFW_KEY_LEFT_SHIFT))
+	{
+		camera.updateCameraPos(CameraDirection::DOWN, dt);
+	}
 
-	}
-	if (Keyboard::keyWentDown(GLFW_KEY_S))
+	double dx = Mouse::getDX();
+	double dy = Mouse::getDY();
+	if (dx != 0 || dy != 0)
 	{
-		trans = glm::translate(trans, glm::vec3(0.0f, -0.01f, 0.0f));
+		camera.updateCameraDirection(dx, dy);
 	}
-	if (Keyboard::keyWentDown(GLFW_KEY_A))
+	double scrollDY = Mouse::getScrollDY();
+	if (scrollDY != 0)
 	{
-		trans = glm::translate(trans, glm::vec3(-0.01f, 0.0f, 0.0f));
-	}
-	if (Keyboard::keyWentDown(GLFW_KEY_D))
-	{
-		trans = glm::translate(trans, glm::vec3(0.01f, 0.0f, 0.0f));
+		camera.updateCameraZoom(scrollDY);
+
 	}
 }
